11aed24c2ee82ea143ff9705a3d7d7869db7f042
Jorge Velasquez <jevelasquez643@misena.edu.co>
2018-03-09 16:48:01 -0500

    A partir de este Momento estoy conectado al GITHUB desde la TERMINAL de mi casa.

--- build/generated-sources/ap-source-output/Controller/UsuariosHV_.java
+++ build/generated-sources/ap-source-output/Controller/UsuariosHV_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.1.v20130918-rNA", date="2018-03-09T12:48:38")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-03-09T16:46:01")
 @StaticMetamodel(UsuariosHV.class)
 public class UsuariosHV_ { 
 
